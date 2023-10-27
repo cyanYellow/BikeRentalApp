fefb0339af45b8587ad28332c239491ac9cc0c40
cyanYellow <124814663+cyanYellow@users.noreply.github.com>
2023-10-26 18:39:00 -0700

    Initial Commit

---
+++ BikeRentalApp/BikeRentalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeRentalAppApp.swift
+//  BikeRentalApp
+//
+//  Created by Willie Green on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BikeRentalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
