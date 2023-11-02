4ca9c70c7347f2d1bf2ca25accdd85f6850c85b3
cyanYellow <124814663+cyanYellow@users.noreply.github.com>
2023-11-02 17:23:19 -0600

    Bike Rental App

--- BikeRentalApp/LandingPage.swift
+++ BikeRentalApp/LandingPage.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
+import AVKit
+
+struct LandingPage: View {
+   
+    //video player variables
+    @State var player = AVPlayer(url: Bundle.main.url(forResource: "BikeBackground", withExtension: "mp4")!)
+    
+    //login variables
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUserName = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var body: some View {
+        NavigationView{
+            ZStack{
+                VideoPlayer(player: player)
+                
+            }
+        }
+        //.navigationBarHidden(true)
+    }
+}
+
+#Preview {
+    LandingPage()
+}
