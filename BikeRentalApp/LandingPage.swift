3fcf0da616baaf1972197b708243371fbde3c47f
cyanYellow <124814663+cyanYellow@users.noreply.github.com>
2023-11-02 19:31:50 -0600

    Update Landing PAge

--- BikeRentalApp/LandingPage.swift
+++ BikeRentalApp/LandingPage.swift
@@ -5,33 +5,65 @@
 //  Created by Willie Green on 10/26/23.
 //
 
-import Foundation
 import SwiftUI
+import AVFoundation
 import AVKit
 
-struct LandingPage: View {
-   
-    //video player variables
-    @State var player = AVPlayer(url: Bundle.main.url(forResource: "BikeBackground", withExtension: "mp4")!)
-    
-    //login variables
-    @State private var username = ""
-    @State private var password = ""
-    @State private var wrongUserName = 0
-    @State private var wrongPassword = 0
-    @State private var showingLoginScreen = false
-    
-    var body: some View {
-        NavigationView{
-            ZStack{
-                VideoPlayer(player: player)
-                
-            }
-        }
-        //.navigationBarHidden(true)
+struct PlayerView: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return LoopingPlayerUIView(frame: .zero)
     }
 }
 
-#Preview {
-    LandingPage()
+class LoopingPlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Load the resource -> h
+        let fileUrl = Bundle.main.url(forResource: "BikeBackground", withExtension: "mp4")!
+        let asset = AVAsset(url: fileUrl)
+        let item = AVPlayerItem(asset: asset)
+        // Setup the player
+        let player = AVQueuePlayer()
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        // Create a new player looper with the queue player and template item
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+        // Start the movie
+        player.play()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
+
+struct LandingPageView: View {
+    var body: some View {
+        ZStack{
+            GeometryReader{ geo in
+                PlayerView()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width, height: geo.size.height+100)
+                    .edgesIgnoringSafeArea(.all)
+                    .overlay(Color.black.opacity(0.2))
+                    .blur(radius: 1)
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct LandingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView()
+    }
 }
